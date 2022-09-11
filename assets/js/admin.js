e0277b4bba11942503737bf1347b91213c670e92
Hilario Raygoza Gutiérrez <hilario.raygoza4946@alumnos.udg.mx>
2022-09-11 18:20:32 -0500

    create product form

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,14 @@
 import { load_menu } from "./product.js";
 
+function createProduct() {
+    let nombre = document.getElementById("nombre").value;
+    let precio = document.getElementById("precio").value;
+    let descripcion = document.getElementById("desc").value;
+    let base = document.getElementById("base").value;
+    
+    console.log(`${nombre} ${precio} ${descripcion} ${base}`);
+}
+
 window.onload = function () {
     load_menu();
 }
